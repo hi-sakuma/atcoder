980fb227f0b12ea64ff4d4726dbc888a96367017
sakuma <sakuma@hs.local>
2019-11-19 00:24:32 +0900

    120C

---
+++ src/ABC/120/C.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    int A[M], B[M];
+    for(int i=0;i<M;i++)
+    {
+        cin >> A[i] >> B[i];
+    }
+
+    cout << 2 * min(i, j) << endl;
+    return 0;
+}
