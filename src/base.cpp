d7ca25cdd57175b86c144a6c00e2bc52750bd8c6
sak-krj <15926535h@gmail.com>
2019-11-23 21:42:35 +0900

    repの引数でカウンタの初期値を指定できるように変更

--- src/base.cpp
+++ src/base.cpp
@@ -1,6 +1,6 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-#define rep(i, n) for(int i=0; i<(n); i++)
+#define rep(i, s, n) for (int i = (s); i < (n); i++)
 
 int main()
 {
