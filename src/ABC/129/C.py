6d458fcc7ff8739d4c6cd70258ec9a5970b59347
sakuma-mindshift <sakuma.hiroki@mindshift.co.jp>
2020-02-04 13:48:49 +0900

    やった

---
+++ src/ABC/129/C.py
@@ -0,0 +1,7 @@
+n, m = map(int, input().split())
+a = [int(input()) for _ in range(m)] + [n+1]
+print(a)
+
+ex_drop = -1
+now = 0
+# for i in range(m+1):
---
+++ src/ABC/138/C.py
@@ -0,0 +1,4 @@
+n = int(input())
+V = sorted(list(map(int, input().split())), reverse = True)
+print(V)
+
