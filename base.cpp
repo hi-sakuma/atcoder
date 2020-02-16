c40dcf695778130571c1725a64cd7fbbbce6d62a
sak-krj <15926535h@gmail.com>
2020-02-16 00:37:38 +0000

    cppテンプレート追加

--- base.cpp
+++ base.cpp
@@ -5,9 +5,16 @@
 using namespace std;
 #define rep(i, s, n) for (int i = (s); i < (n); i++)
 
+
 int main()
 {
     int x;
     cin >> x;
+
+    int n, m;
+    cin >> n >> m;
+    int a[n];
+    rep(i, 0, n) cin >> a[i];
+
     cout << x << endl;
 }
